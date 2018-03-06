d7e8e594623c9b1cf9f0639ed6ee34b645b1427c
jlljll <18328073224@163.com>
2018-03-06 10:53:31 +0800

    first

---
+++ stop.php
@@ -0,0 +1,35 @@
+<?php
+$data='{
+  "data": [
+    {
+      "name": "stop1",
+      "id": 232
+    },
+    {
+      "name": "stop2",
+      "id": 30
+    },
+    {
+      "name": "stop3",
+      "id": 324
+    },
+    {
+      "name": "stop4",
+      "id": 166
+    },
+    {
+      "name": "stop5",
+      "id": 208
+    },
+    {
+      "name": "stop6",
+      "id": 279
+    },
+    {
+      "name": "stop7",
+      "id": 40
+    }
+    ]}';
+echo $data;
+
+?>
